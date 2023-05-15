6de6572c23cf8af8c1b3af1d15e47407c998cd18
Evan Sarkozi <prefsuite@Live.ca>
2023-05-15 14:54:27 -0700

    Checkpoint - End

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     public float jumpVelocity = 12.5f;
     public int health = 3;
 
+    public CollisionChecker bottomChecker;
+    public CollisionChecker leftChecker;
+    public CollisionChecker rightChecker;
+
     private Rigidbody2D m_rigidbody;
     private Animator m_animator;
     private SpriteRenderer m_renderer;
@@ -25,8 +29,11 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 targetVelocity = m_rigidbody.velocity;
 
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
+        if(bottomChecker.Colliding && Input.GetKeyDown(KeyCode.Space)) {
+            targetVelocity.y = jumpVelocity;
+        }
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
             targetVelocity.x = -playerSpeed;
         }
         else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
@@ -40,5 +47,9 @@ public class PlayerController : MonoBehaviour
 
         m_rigidbody.velocity = targetVelocity;
         m_animator.SetBool("Running", m_rigidbody.velocity.x != 0);
+        m_animator.SetBool("Jumping", !bottomChecker.Colliding);
+        if(targetVelocity.x != 0) {
+            m_renderer.flipX = targetVelocity.x < 0;
+        }
     }
 }
